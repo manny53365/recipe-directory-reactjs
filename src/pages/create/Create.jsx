c27a68d1697fe5f3b7ad6ca8b2e3d8506d3ef6f5
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-03 22:08:38 +0530

    added create form and some styling

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,9 +1,52 @@
+import { useState } from 'react';
+
 import './Create.css'
 
 export default function Create() {
+
+    const [title, setTitle] = useState('');
+    const [cookingTime, setCookingTime] = useState('');
+    const [method, setMethod] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(title, method, cookingTime)
+    };
+
     return(
-        <div>
-            Create
+        <div className='create'>
+            <h2 className='page-title'>Add a new recipe</h2>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Recipe title:</span>
+                </label>
+                <input
+                type="text"
+                onChange={(event) => setTitle(event.target.value)}
+                value={title}
+                required
+                />
+                <label>
+                    <span>Method:</span>
+                </label>
+                <textarea
+                onChange={(event) => setMethod(event.target.value)}
+                value={method}
+                required
+                />
+                <label>
+                    <span>Cooking time:</span>
+                </label>
+                <input
+                type="number"
+                onChange={(event) => setCookingTime(event.target.value)}
+                value={cookingTime}
+                required
+                />
+
+                <button className='btn'>Submit</button>
+            </form>
         </div>
     )
 };
