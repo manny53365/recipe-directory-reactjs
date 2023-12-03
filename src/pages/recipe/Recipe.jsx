a39c7199d6e6bb7c2fe1666dc9c0a4626a5c0d65
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-03 21:47:23 +0530

    added route parameter handling for recipes and added some styling for the output of the data to the page

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,9 +1,27 @@
+import { useParams } from 'react-router-dom';
+
+import { useFetch } from '../../hooks/useFetch';
+
 import './Recipe.css';
 
 export default function Recipe() {
+
+    const { id } = useParams()
+    const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+
     return(
-        <div>
-            Recipe
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {recipe && 
+            <div>
+                <h2 className='page-title'>{recipe.title}</h2>
+                <p>Takes {recipe.cookingTime} to cook.</p>
+                <ul>
+                    {recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+                </ul>
+                <p className='method'>{recipe.method}</p>
+            </div>}
         </div>
     )
 };
