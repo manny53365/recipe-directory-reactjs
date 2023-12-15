46002f3d9e84a9944d3292c7843aaab7869177ea
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-15 13:27:08 +0530

    added colour selector to the main page for the user to allow user to customize their colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import Searchbar from './Searchbar';
 
 export default function Navbar() {
 
-    const { color, changeColor } = useTheme();
+    const { color} = useTheme();
 
     return (
         <div className='navbar' style={{background: color}}>
-            <nav onClick={() => changeColor('pink')}>
+            <nav>
                 <Link to='/' className='brand'>
                     <h1>Cooking Recipes</h1>
                 </Link>
