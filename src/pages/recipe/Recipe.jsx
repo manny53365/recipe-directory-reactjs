4b8e5949d7217c132b9a0decb8f3a6cb11987124
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-15 14:13:56 +0530

    added styling to update color scheme if the user selects dark mode

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -3,14 +3,16 @@ import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
 
 import './Recipe.css';
+import { useTheme } from '../../hooks/useTheme';
 
 export default function Recipe() {
 
     const { id } = useParams()
     const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+    const {mode} = useTheme();
 
     return(
-        <div className='recipe'>
+        <div className={`recipe ${mode}`}>
             {error && <p className='error'>{error}</p>}
             {isPending && <p className='loading'>Loading...</p>}
             {recipe && 
