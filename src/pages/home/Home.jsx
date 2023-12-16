247874e9e121be897967406f2683252ff2b673d0
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-16 15:43:42 +0530

    added conection to firebase to pull the collection data from the db

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,7 +15,22 @@ export default function Home() {
     useEffect(() => {
         setIsPending(true);
 
-        projectFirestore.collection('recipes').get().then(snapshot => console.log(snapshot));
+        projectFirestore.collection('recipes').get().then(snapshot => {
+            if(snapshot.empty){
+                setError('No recipes to load');
+                setIsPending(false);
+            } else {
+                let results = [];
+                snapshot.docs.forEach(doc => {
+                    results.push({ id: doc.id, ...doc.data() })
+                })
+                setData(results);
+                setIsPending(false);
+            }
+        }).catch(err => {
+            setError(err.message);
+            setIsPending(false);
+        });
     }, [])
 
     // const {data, isPending, error} = useFetch('http://localhost:3000/recipes');
