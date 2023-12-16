d920cf77fb4926c4f6e5211dd9c0e4bcaa028a1a
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-16 16:25:49 +0530

    added functionality to serve up the recipe data from firebase using a get request from the app

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -15,7 +15,7 @@ export default function RecipeList({ recipes }) {
                 {recipes.map(recipe => (
                     <div key={recipe.id} className={`card ${mode}`}>
                         <h3>{recipe.title}</h3>
-                        <p>{recipe.cookingTime} to make.</p>
+                        <p>{recipe.cookingTime} minutes to make.</p>
                         <div>{recipe.method.substring(0,100)}...</div>
                         <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
                     </div>
--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 
-import { useFetch } from '../../hooks/useFetch';
+// import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
+import { useEffect, useState } from 'react';
 
 import './Recipe.css';
 import { useTheme } from '../../hooks/useTheme';
@@ -8,8 +10,26 @@ import { useTheme } from '../../hooks/useTheme';
 export default function Recipe() {
 
     const { id } = useParams()
-    const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
-    const {mode} = useTheme();
+    // const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+    const { mode } = useTheme();
+
+    const [recipe, setRecipe] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        setIsPending(true);
+
+        projectFirestore.collection('recipes').doc(id).get().then(docSnapshot => {
+            if (docSnapshot.exists){
+                setIsPending(false);
+                setRecipe(docSnapshot.data());
+            } else {
+                setIsPending(false);
+                setError('Could not find the recipe');
+            }
+        })
+    }, [id])
 
     return(
         <div className={`recipe ${mode}`}>
